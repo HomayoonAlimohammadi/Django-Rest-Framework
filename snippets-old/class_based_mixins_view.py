e6c5a9c9bd7dd4b7eaaae16f5254a1f404152c5b
Homayoon <homayoonalimohammadi@gmail.com>
2022-03-26 22:32:48 +0430

    Update snippets/views.py, Add snippets-old

---
+++ snippets-old/class_based_mixins_view.py
@@ -0,0 +1,42 @@
+from .models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import mixins, generics
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    '''
+    List all code snippets, or create a new snippet.
+    '''
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    '''
+    Retrieve, update or delete a code snippet.
+    '''
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    
+
+    
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import * 
+from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', snippet_list),
-    path('<int:pk>/', snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
